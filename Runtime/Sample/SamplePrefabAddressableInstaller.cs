577faa03199e51c9f6c7c457ffde08402432aa61
Sercan Altun <altun.sercan@gmail.com>
2020-06-03 22:53:59 -0400

    Add usage samples

---
+++ Runtime/Sample/SamplePrefabAddressableInstaller.cs
@@ -0,0 +1,38 @@
+﻿using AssetLoading;
+using AssetLoading.Installers;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Zenject;
+
+public class SamplePrefabAddressableInstaller : AddressablePreloadListInstaller<SamplePrefabAddressableInstaller>
+{
+    public AssetReferenceGameObject prefabToLoad;
+	   
+    protected override void InstallInternalBindings()
+    {
+        // GameObjectAssetLoader needs root to instantiate prefab in.
+        Container.Bind<GameObject>().FromResolveGetter<Context>(context => context.gameObject);
+        Container.Bind<GameObjectAssetLoader>().FromResolve(nameof(prefabToLoad));
+        Container.BindInterfacesTo<SampleRotator>().AsSingle();
+    }
+
+    private class SampleRotator : IInitializable
+    {
+        private GameObjectAssetLoader loader;
+        private GameObject loadedGameObject;
+
+        public SampleRotator(GameObjectAssetLoader loader)
+        {
+            this.loader = loader;
+        }
+        
+        public void Initialize()
+        { 
+            loadedGameObject = loader.LoadedAsset;
+            
+            loadedGameObject.transform.localPosition = new Vector3(0, 1, 0);
+        }
+
+    }
+}
